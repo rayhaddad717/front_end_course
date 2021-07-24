2dce6f721f3dd1dec106afd032b00c501a3e3cc3
rayhaddad717 <rayghaddad@gmail.com>
2021-07-24 15:07:41 +0300

    Clock

--- Clock/script.js
+++ Clock/script.js
@@ -8,9 +8,29 @@ const hourHand=document.querySelector('[data-hour-hand]')
 const minuteHand=document.querySelector('[data-minute-hand]')
 const secondHand=document.querySelector('[data-second-hand]')
 
+const s=document.getElementById('switch');
+
+
+
+
+
+
+const cb = document.getElementById('accept');
+if(cb.checked){
+alert(`Key pressed\r\n key code value: `);}
+
+
+
+
+
+
+
+
 
 function setClock(){
     const currentDate=new Date()
+    
+alert(`Key pressed\r\n key code value: `)
     const secondsRatio= currentDate.getSeconds() / 60
     const minutesRatio=(secondsRatio + currentDate.getMinutes())/ 60    
     const hoursRatio=(minutesRatio + currentDate.getHours())/12
@@ -18,12 +38,10 @@ function setClock(){
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
 }
-function sync(){
-  if(checkBox.checked) {
-    setClock();
- }else{
-    alert('unchecked');
- }
+function sync(element){
+    if(element.checked){
+    setClock();}
+    else{setClock();}
    // setInterval(setClock,1000)
 }
 function setRotation(element,rotationRatio){
